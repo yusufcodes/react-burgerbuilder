3583bcc4a8a8855f04aae79509ba8c43a9ee9cc0
yusufcodes <yusufcodes@outlook.com>
2020-05-29 08:15:15 +0100

    New project due to issues earlier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import styles from './css/Button.module.css';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <button className={styles.error}>Error Button</button>
+    </div>
+  );
 }
 
 export default App;
