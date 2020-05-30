8b5e4f24db6baa88a1d9de8851d6d4d582c2a55b
yusufcodes <yusufcodes@outlook.com>
2020-05-30 08:28:38 +0100

    Created BuildControls and BuildControl BuildControls returns a set of singular BuildControl components, each loaded based on the ingredients we need. Styling provided by instructor.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'},
+]
+
+const buildControls = props => {
+    return (
+    <div className={classes.BuildControls}>
+        {
+        controls.map((currentControl) => {
+            return <BuildControl key={currentControl.label} label={currentControl.label} type={currentControl.type} />
+        })
+        }
+    </div>
+    )
+}
+
+export default buildControls;
