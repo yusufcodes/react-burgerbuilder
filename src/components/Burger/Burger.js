4875b87d9317c13507c4abd9494398aa6fd32e45
yusufcodes <yusufcodes@outlook.com>
2020-05-30 07:31:04 +0100

    Reading ingredients from state to dynamically build the burger Note: This uses two map methods in Burder.js and it took me some time to wrap my head around it. I commented it as thoroughly as possible - hoping to understand further examples!

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,10 +3,36 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css'
 
 const burger = (props) => {
+    // Ingredients: Object -> Array
+    // This retrieves each key so we will end up with:
+    // ['salad', 'bacon', 'cheese', ...]
+
+
+
+    /* transformedIngredients: We want a way to know how many of each ingredient in the ingredients 
+    object we have. First, we start by grabbing only the keys */
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        // Map #1: Each ingredient now becomes an Array with some undefined elements.
+        // We do not care about the fact that is is not defined but rather, the length of such element.
+        // e.g. 'salad: 1' from props.ingredients -> Array(1), because props.ingredients[igKey] renders the value of 1.
+        /* Example Output: 
+        0: [undefined]
+        1: (2) [undefined, undefined]
+        2: [undefined] */
+        return [...Array(props.ingredients[igKey])]
+        // Map #2: For each undefined item in our new arrays, we create a new BurgerIngredient component
+        // passing in the 'igKey' i.e. the ingredient we need. The amounts are dictated from the output of the
+        // first Mapping, so all we do is loop through each array of undefined to determine how many we need.
+        .map((_, index) => {
+            return <BurgerIngredient key={igKey + index} type={igKey} />
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
