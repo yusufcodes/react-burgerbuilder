7b61aaadc034a1db303705bb73e756361cee72c5
yusufcodes <yusufcodes@outlook.com>
2020-06-03 21:29:05 +0100

    Added a backdrop to the modal which can be clicked to escape it

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,6 +83,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         // Boolean values dictating if particular button should be enabled/disabled
         const disabledInfo = {
@@ -95,7 +99,9 @@ class BurgerBuilder extends Component {
 
         return (
             <React.Fragment>
-                <Modal show={this.state.purchasing}>
+                <Modal
+                show={this.state.purchasing}
+                modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
