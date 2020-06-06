8378781de319940de93c79383a80c8dad99f1caa
yusufcodes <yusufcodes@outlook.com>
2020-06-06 12:13:44 +0100

    Created reuseable navigation items with nav bar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Logo from '../../../components/Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.module.css';
+
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
