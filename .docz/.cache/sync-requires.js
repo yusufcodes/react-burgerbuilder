cf8aebc8fe2903f151d5045cc584fa3f95770aca
yusufcodes <yusufcodes@outlook.com>
2020-06-08 07:06:57 +0100

    Added Axios to project with a new instance

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\YUSUF\\Documents\\Self Study\\React\\React - The Complete Guide\\Course Projects\\burger\\.docz\\.cache\\dev-404-page.js"))),
+  "component---readme-md": hot(preferDefault(require("C:\\Users\\YUSUF\\Documents\\Self Study\\React\\React - The Complete Guide\\Course Projects\\burger\\README.md"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\YUSUF\\Documents\\Self Study\\React\\React - The Complete Guide\\Course Projects\\burger\\.docz\\src\\pages\\404.js")))
+}
+
