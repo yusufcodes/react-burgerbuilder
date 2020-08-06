e4402f315f5f6d1ad1ab3d2f3260f9468f35d8a7
yusufcodes <yusufcodes@outlook.com>
2020-08-06 20:48:21 +0100

    Created Orders and Order components, for new orders page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import './App.css';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
 
@@ -14,6 +15,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout}/>
+            <Route path="/orders" component={Orders}/>
             <Route path="/" exact component={BurgerBuilder}/>
           </Switch>
         </Layout>
