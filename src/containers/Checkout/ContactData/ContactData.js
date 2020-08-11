9a3e4b3c3e57221bec071f0ab4e29e68f5f15054
yusufcodes <yusufcodes@outlook.com>
2020-08-11 12:50:29 +0100

    Used the JS Config object to dynamically create our custom Input elements

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,11 +83,24 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+
+        // Creating an array of JavaScript Objects, so we can iterate over it
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (<form>
-            <Input elementType="..." elementConfig="..." value="..."></Input>
-            <Input inputtype="input" type="email" name="email" placeholder="Email"></Input>
-            <Input inputtype="input" type="text" name="street" placeholder="Street"></Input>
-            <Input inputtype="input" type="text" name="postcode" placeholder="Postcode"></Input>
+            {formElementsArray.map(formElement => (
+                <Input 
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}/>
+            ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if (this.state.loading) {
