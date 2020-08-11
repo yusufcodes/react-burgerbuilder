09fbb558590ef93cf47e50c8a9c59b8ba751e743
yusufcodes <yusufcodes@outlook.com>
2020-08-11 13:02:58 +0100

    Created a custom select type, to show a dropdown of values

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -12,8 +12,20 @@ const input = (props) => {
         case('textarea'):
             inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
             break;
-            default:
-                inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+        case('select'):
+            inputElement = (
+            <select className={classes.InputElement}
+            value={props.value}>
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>
+        )
+            break;
+        default:
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
     }
     
     return (
