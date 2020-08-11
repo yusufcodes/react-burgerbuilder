6021496bd03f74b7296e4cf207102f5f5b681dcf
yusufcodes <yusufcodes@outlook.com>
2020-08-11 14:01:53 +0100

    Used form data entered by the user and now submit it to Firebase database

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,9 +67,17 @@ class ContactData extends Component {
         event.preventDefault();
         this.setState({loading: true});
 
+        const formData = {};
+
+        // Data transformation: Extracting only the data we need from the original object
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,            
+            price: this.props.totalPrice,
+            orderData: formData         
         };
 
         axios.post('/orders.json', order)
@@ -118,7 +126,7 @@ class ContactData extends Component {
             });
         }
 
-        let form = (<form>
+        let form = (<form onSubmit={this.orderHandler}>
             {formElementsArray.map(formElement => (
                 <Input 
                 key={formElement.id}
@@ -127,7 +135,7 @@ class ContactData extends Component {
                 value={formElement.config.value}
                 changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
             ))}
-            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            <Button btnType="Success">ORDER</Button>
         </form>);
         if (this.state.loading) {
             form = <Spinner />
